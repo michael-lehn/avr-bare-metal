c59ebaa77827901c31aebcf9ff7d353a6542653b
Michael C. Lehn <michel.lehn@uni-ulm.de>
2025-05-07 10:10:25 +0200

    Simple example added

---
+++ asm/led_pin2.s
@@ -0,0 +1,8 @@
+        sbi     0x0a, 2     ; Pin 2 als Ausgang konfigurieren
+
+loop:   sbi     0x0b, 2     ; Pin 2 auf HIGH setzen
+        nop		    ; Nichts machen
+        nop		    ; Nichts machen
+        cbi     0x0b, 2     ; Pin 2 auf LOW setzen
+	rjmp    loop        ; Zurück zur Marke 'loop (d.h. 4 Befehele zurück)
+
